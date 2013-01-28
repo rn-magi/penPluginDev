2e0e0b54013e552e27fc3f3e3408d9e512539b92
Ryota Nakamura <rn_magi@zeus.eonet.ne.jp>
2013-01-29 00:06:14 +0900

    [New]
    プラグイン関数の追加
     * str2Half(文字列)
      -> 全角英数字を半角英数字に変換する
     * str2Double(文字列)
      -> 半角英数字を全角英数字に変換する

---
+++ penPluginDev/src/charMorphing.java
@@ -0,0 +1,37 @@
+public class charMorphing implements penPlugin {
+	public charMorphing(){
+		
+	}
+	
+	public String str2Half(String str){
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < str.length(); i++){
+			int c = (int)str.charAt(i);
+			if((c >= 65345 && c <= 65370)
+				|| (c >= 65313 && c <= 65338)
+				|| (c >= 65296 && c <= 65305)){
+				c -= 65248;
+			}
+			sb.append((char)c);
+		}
+		return sb.toString();
+	}
+	
+	public String str2Double(String str){
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < str.length(); i++){
+			int c = (int)str.charAt(i);
+			if((c >= 97 && c <= 122)
+				|| (c >= 65 && c <= 90)
+				|| (c >= 48 && c <= 57)){
+				c += 65248;
+			}
+			sb.append((char)c);
+		}
+		return sb.toString();
+		
+	}
+	
+	public void destruction() {
+	}
+}
