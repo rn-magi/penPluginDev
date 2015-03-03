d6147ac00d977cc02fed956b031139bfe8a9acc9
Ryota Nakamura <rn_magi@zeus.eonet.ne.jp>
2015-03-04 08:35:15 +0900

    [New] jSSCを利用したFirmata protocol用プラグイン

---
+++ FirmataJSSC/src/ArduinoControl.java
@@ -0,0 +1,77 @@
+import org.firmata.*;
+
+public class ArduinoControl implements penPlugin {
+	private Arduino arduino = null;
+	
+	public ArduinoControl() {
+		
+	}
+	
+	public void openPort(String port) {
+		if(arduino == null) {
+				arduino = new Arduino(port);
+		}
+	}
+	
+	public void closePort() {
+		if(arduino != null) {
+			arduino.dispose();
+		}
+		arduino = null;
+	}
+	
+	public String portList(int i) {
+		return Arduino.list()[i];
+	}
+	
+	public int portListLength() {
+		return Arduino.list().length;
+	}
+	
+	public void pinMode(int pin, String mode) {
+		mode.toUpperCase();
+		if(arduino != null) {
+			if(mode.equals("INPUT")) {
+				arduino.pinMode(pin, Arduino.INPUT);
+			} else if(mode.equals("OUTPUT")) {
+				arduino.pinMode(pin, Arduino.OUTPUT);
+			}
+		}
+	}
+	
+	public int digitalRead(int pin) {
+		if(arduino != null) {
+			return arduino.digitalRead(pin);
+		}
+		return 0;
+	}
+	
+	public void digitalWrite(int pin, int value) {
+		if(arduino != null) {
+			arduino.digitalWrite(pin, value);
+		}
+	}
+	
+	public int analogRead(int pin) {
+		if(arduino != null) {
+			return arduino.analogRead(pin);
+		}
+		return 0;
+	}
+	
+	public void analogWrite(int pin, int value) {
+		if(arduino != null) {
+			arduino.analogWrite(pin, value);
+		}
+	}
+	
+	public void init() {
+		
+	}
+	
+	public void destruction() {
+		if(arduino != null) {
+			closePort();
+		}
+	}
+}
